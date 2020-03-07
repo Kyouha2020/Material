8b5c31691f9317c46e729b5fe06588d28cdb3c8f
Kyouha2020 <kyouha@outlook.com>
2020-03-07 16:56:05 +0800

    Mar 7, 2020

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,4 @@
-import('../src/components/ripple/Ripple.js')
+import('../src/design/interaction/Ripple.js')
 
 CSS.paintWorklet.addModule('../../src/worklets/corner-shape.js')
 CSS.paintWorklet.addModule('../../src/worklets/ripple.js')
