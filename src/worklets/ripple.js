70420d1bc7d3c5a7eddddcad6c859c30305b3ce6
Kyouha2020 <kyouha@outlook.com>
2020-03-10 14:41:49 +0800

    Mar 10, 2020

--- src/worklets/ripple.js
+++ src/worklets/ripple.js
@@ -1,10 +1,11 @@
 if (typeof registerPaint !== 'undefined') registerPaint('ripple', class {
     static get inputProperties() {
-        return ['--ripple-color', '--animation-tick', '--ripple-x', '--ripple-y', '--ripple-speed']
+        return ['--ripple-color', '--ripple-opacity', '--animation-tick', '--ripple-x', '--ripple-y', '--ripple-speed']
     }
 
     paint(ctx, geom, properties) {
         const rippleColor = properties.get('--ripple-color').toString(),
+            rippleOpacity = properties.get('--ripple-opacity').toString().replace(/[ %]/g, '') / 100,
             x = parseFloat(properties.get('--ripple-x').toString()),
             y = parseFloat(properties.get('--ripple-y').toString()),
             speed = parseFloat((properties.get('--ripple-speed') || '').toString()) || 1
@@ -14,7 +15,7 @@ if (typeof registerPaint !== 'undefined') registerPaint('ripple', class {
         if (tick > 1000) tick = 1000
 
         ctx.fillStyle = rippleColor
-        ctx.globalAlpha = 1 - tick / 1000
+        ctx.globalAlpha = rippleOpacity - tick / 1000 * rippleOpacity
         ctx.arc(x, y, geom.width * tick / 1000, 0, 2 * Math.PI)
         ctx.fill()
     }
