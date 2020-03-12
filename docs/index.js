5b23cea380c3d08248deb9aa00640f413f6a13fd
Kyouha2020 <kyouha@outlook.com>
2020-03-12 14:53:46 +0800

    Mar 12, 2020: remove ripple

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,3 @@
-import('../src/components/ripple/Ripple.js')
-
 if (typeof CSS.paintWorklet !== 'undefined') {
     CSS.paintWorklet.addModule('../../src/worklets/corner-shape.js')
     CSS.paintWorklet.addModule('../../src/worklets/ripple.js')
