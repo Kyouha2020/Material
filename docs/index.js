8fdf05b01d9dd802e018230d75b18f2f57596c17
Kyouha2020 <kyouha@outlook.com>
2020-03-12 22:23:04 +0800

    Mar 12, 2020: rtl support

--- docs/index.js
+++ docs/index.js
@@ -7,9 +7,20 @@ const themeToggle = document.querySelector('#theme-toggle')
 themeToggle.addEventListener('click', () => {
     if (!document.documentElement.hasAttribute('dark')) {
         document.documentElement.setAttribute('dark', '')
-        themeToggle.innerHTML = `Light Theme`
+        themeToggle.innerHTML = `Dark Theme is on`
     } else {
         document.documentElement.removeAttribute('dark')
-        themeToggle.innerHTML = `Dark Theme`
+        themeToggle.innerHTML = `Dark Theme is off`
+    }
+})
+
+const dirToggle = document.querySelector('#dir-toggle')
+dirToggle.addEventListener('click', () => {
+    if (!document.documentElement.hasAttribute('dir')) {
+        document.documentElement.setAttribute('dir', 'rtl')
+        dirToggle.innerHTML = `RTL is on`
+    } else {
+        document.documentElement.removeAttribute('dir')
+        dirToggle.innerHTML = `RTL is off`
     }
 })
