96f629cd052986fd35e6e0e59a80e75a16ade828
Kyouha2020 <kyouha@outlook.com>
2020-03-13 15:05:30 +0800

    update theme / dir toggle

--- docs/index.js
+++ docs/index.js
@@ -7,9 +7,11 @@ themeToggle.addEventListener('click', () => {
     if (!document.documentElement.hasAttribute('dark')) {
         document.documentElement.setAttribute('dark', '')
         themeToggle.innerHTML = `<i class="material-icons">brightness_7</i>`
+        themeToggle.setAttribute('title', 'light mode')
     } else {
         document.documentElement.removeAttribute('dark')
         themeToggle.innerHTML = `<i class="material-icons">brightness_4</i>`
+        themeToggle.setAttribute('title', 'dark mode')
     }
 })
 
@@ -18,8 +20,10 @@ dirToggle.addEventListener('click', () => {
     if (!document.documentElement.hasAttribute('dir')) {
         document.documentElement.setAttribute('dir', 'rtl')
         dirToggle.innerHTML = `<i class="material-icons">navigate_next</i>`
+        dirToggle.setAttribute('title', 'LTR mode')
     } else {
         document.documentElement.removeAttribute('dir')
         dirToggle.innerHTML = `<i class="material-icons">navigate_before</i>`
+        dirToggle.setAttribute('title', 'RTL mode')
     }
 })
