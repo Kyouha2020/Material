857805c44b9ab71e285e100053ed9a4b090d5ad6
Kyouha2020 <kyouha@outlook.com>
2020-03-15 12:30:11 +0800

    chore

--- docs/index.js
+++ docs/index.js
@@ -3,27 +3,29 @@ if (typeof CSS.paintWorklet !== 'undefined') {
 }
 
 const themeToggle = document.querySelector('#theme-toggle')
-themeToggle.addEventListener('click', () => {
-    if (!document.documentElement.hasAttribute('dark')) {
-        document.documentElement.setAttribute('dark', '')
-        themeToggle.innerHTML = `<i class="material-icons">brightness_7</i>`
-        themeToggle.setAttribute('title', 'light mode')
-    } else {
-        document.documentElement.removeAttribute('dark')
-        themeToggle.innerHTML = `<i class="material-icons">brightness_4</i>`
-        themeToggle.setAttribute('title', 'dark mode')
-    }
-})
+if (themeToggle !== null)
+    themeToggle.addEventListener('click', () => {
+        if (!document.documentElement.hasAttribute('dark')) {
+            document.documentElement.setAttribute('dark', '')
+            themeToggle.innerHTML = `<i class="material-icons">brightness_7</i>`
+            themeToggle.setAttribute('title', 'light mode')
+        } else {
+            document.documentElement.removeAttribute('dark')
+            themeToggle.innerHTML = `<i class="material-icons">brightness_4</i>`
+            themeToggle.setAttribute('title', 'dark mode')
+        }
+    })
 
 const dirToggle = document.querySelector('#dir-toggle')
-dirToggle.addEventListener('click', () => {
-    if (!document.documentElement.hasAttribute('dir')) {
-        document.documentElement.setAttribute('dir', 'rtl')
-        dirToggle.innerHTML = `<i class="material-icons">navigate_next</i>`
-        dirToggle.setAttribute('title', 'LTR mode')
-    } else {
-        document.documentElement.removeAttribute('dir')
-        dirToggle.innerHTML = `<i class="material-icons">navigate_before</i>`
-        dirToggle.setAttribute('title', 'RTL mode')
-    }
-})
+if (dirToggle !== null)
+    dirToggle.addEventListener('click', () => {
+        if (!document.documentElement.hasAttribute('dir')) {
+            document.documentElement.setAttribute('dir', 'rtl')
+            dirToggle.innerHTML = `<i class="material-icons">navigate_next</i>`
+            dirToggle.setAttribute('title', 'LTR mode')
+        } else {
+            document.documentElement.removeAttribute('dir')
+            dirToggle.innerHTML = `<i class="material-icons">navigate_before</i>`
+            dirToggle.setAttribute('title', 'RTL mode')
+        }
+    })
