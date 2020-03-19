a019dc1aaa03e4fc68a7040668cbfc9a1a164e6f
Kyouha2020 <kyouha@outlook.com>
2020-03-19 13:56:11 +0800

    add toggle button

--- docs/index.js
+++ docs/index.js
@@ -25,3 +25,12 @@ if (dirToggle !== null)
             dirToggle.setAttribute('title', 'RTL mode')
         }
     })
+
+const toggleButtons = document.querySelectorAll('.mtrl-button--toggle')
+toggleButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.classList.contains('mtrl-button--toggle--active'))
+            button.classList.remove('mtrl-button--toggle--active')
+        else button.classList.add('mtrl-button--toggle--active')
+    })
+})
